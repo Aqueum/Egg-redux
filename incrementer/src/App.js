6b3631fb581759c376a4d28d7c1f1d02c43d7f81
Aqueum <martin.currie@aqueum.com>
2018-03-26 11:37:10 +0100

    feat: Add counter and store

--- incrementer/src/App.js
+++ incrementer/src/App.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counter);
 
 class App extends Component {
   render() {
