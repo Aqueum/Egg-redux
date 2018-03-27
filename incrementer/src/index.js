b1353d9dfcf3706ca5af91183a88457dcfdc16bd
Aqueum <martin.currie@aqueum.com>
2018-03-27 11:24:49 +0100

    feat: Add todo

--- incrementer/src/index.js
+++ incrementer/src/index.js
@@ -6,7 +6,21 @@ import deepFreeze from 'deep-freeze';
 
 // Writing a Todo List Reducer
 
-const todos = (state = [], action) => {};
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+    default:
+      return state;
+  }
+};
 
 const testAddTodo = () => {
   const stateBefore = [];
