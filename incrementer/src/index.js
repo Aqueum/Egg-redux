4010c9b02a4529fbd46da190b6279182d680088f
Aqueum <martin.currie@aqueum.com>
2018-03-27 23:40:19 +0100

    func: Add todo completion

--- incrementer/src/index.js
+++ incrementer/src/index.js
@@ -148,19 +148,40 @@ class TodoApp extends Component {
   render() {
     return (
       <div>
+        <input
+          ref={node => {
+            this.input = node;
+          }}
+        />
         <button
           onClick={() => {
             store.dispatch({
               type: 'ADD_TODO',
-              text: 'Test',
+              text: this.input.value,
               id: nextTodoID++
             });
+            this.input.value = '';
           }}
         >
           Add Todo
         </button>
         <ul>
-          {this.props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+          {this.props.todos.map(todo => (
+            <li
+              key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                });
+              }}
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none'
+              }}
+            >
+              {todo.text}
+            </li>
+          ))}
         </ul>
       </div>
     );
